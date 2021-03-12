e12e1998250bab72dc19608fa28016615bea0380
CentralTang <yiqiao1996@gmail.com>
2021-03-12 11:11:45 +0800

    gs

--- gs.py
+++ gs.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 from deepctr.models import DeepFM
 from deepctr.feature_column import SparseFeat, DenseFeat, get_feature_names
 data = pd.read_csv('./criteo_sample.txt')
-sparse_features = ['C' + str(i)foriinrange(1, 27)]
-dense_features = ['I'+str(i)foriinrange(1, 14)]
+sparse_features = ['C' + str(i)for i in range(1, 27)]
+dense_features = ['I'+str(i)for i in range(1, 14)]
 data[sparse_features] = data[sparse_features].fillna('-1', )
 data[dense_features] = data[dense_features].fillna(0,)
 target = ['label']
